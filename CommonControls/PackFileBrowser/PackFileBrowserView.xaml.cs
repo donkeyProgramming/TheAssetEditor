598265a859d9c0b4264c8b1090d13c3a849e992d
ole kristian homelien <ole_kristian92@hotmail.com>
2021-02-12 13:59:05 +0100

    Changes

---
+++ CommonControls/PackFileBrowser/PackFileBrowserView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommonControls.PackFileBrowser
+{
+    /// <summary>
+    /// Interaction logic for PackFileBrowserView.xaml
+    /// </summary>
+    public partial class PackFileBrowserView : UserControl
+    {
+        public PackFileBrowserView()
+        {
+            InitializeComponent();
+        }
+
+        public ContextMenu CustomContextMenu
+        {
+            get { return (ContextMenu)GetValue(CustomContextMenuProperty); }
+            set { SetValue(CustomContextMenuProperty, value); }
+        }
+
+        public static readonly DependencyProperty CustomContextMenuProperty = DependencyProperty.Register("CustomContextMenu", typeof(ContextMenu), typeof(PackFileBrowserView), new UIPropertyMetadata(null));
+
+    }
+}
