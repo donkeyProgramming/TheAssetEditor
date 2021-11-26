d15dce8724b11015b096885528eb7c3d625fb278
ole kristian homelien <ole_kristian92@hotmail.com>
2021-11-26 09:58:22 +0100

    v0.19

--- AssetEditor/App.xaml.cs
+++ AssetEditor/App.xaml.cs
@@ -21,7 +21,7 @@ namespace AssetEditor
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            VersionChecker.CheckVersion("v0.18");
+            VersionChecker.CheckVersion("v0.19");
 
             Current.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(DispatcherUnhandledExceptionHandler);
 
