8d499dc1b2e8a3d5a21050cab922b011a5ee201c
Tibor Ilić <tilic5@gmail.com>
2022-01-18 19:53:43 +0100

    move BindingProxy to own file

--- AssetEditor/ViewModels/MainViewModel.cs
+++ AssetEditor/ViewModels/MainViewModel.cs
@@ -20,25 +20,6 @@ using TextureEditor.ViewModels;
 
 namespace AssetEditor.ViewModels
 {
-    // Helper when binding inside ContextMenu
-    // https://stackoverflow.com/questions/3583507/wpf-binding-a-contextmenu-to-an-mvvm-command
-    public class BindingProxy : Freezable
-    {
-        protected override Freezable CreateInstanceCore()
-        {
-            return new BindingProxy();
-        }
-
-        public object Data
-        {
-            get { return (object)GetValue(DataProperty); }
-            set { SetValue(DataProperty, value); }
-        }
-
-        public static readonly DependencyProperty DataProperty =
-            DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
-    }
-
     class MainViewModel : NotifyPropertyChangedImpl, IEditorCreator
     {
         ILogger _logger = Logging.Create<MainViewModel>();
