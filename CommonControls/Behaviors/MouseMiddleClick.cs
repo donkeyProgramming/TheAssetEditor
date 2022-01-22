ef48882c4161cc1a6949bc7678dd5b83d74ea389
Tibor Ilić <tilic5@gmail.com>
2022-01-22 17:46:56 +0100

    close all/left/right tabs and close tab on middle click

---
+++ CommonControls/Behaviors/MouseMiddleClick.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CommonControls.Behaviors
+{
+    public class MouseMiddleClick
+    {
+        public static DependencyProperty CommandProperty =
+            DependencyProperty.RegisterAttached("Command",
+            typeof(ICommand),
+            typeof(MouseMiddleClick),
+            new UIPropertyMetadata(CommandChanged));
+
+        public static DependencyProperty CommandParameterProperty =
+            DependencyProperty.RegisterAttached("CommandParameter",
+                                                typeof(object),
+                                                typeof(MouseMiddleClick),
+                                                new UIPropertyMetadata(null));
+
+        public static void SetCommand(DependencyObject target, ICommand value)
+        {
+            target.SetValue(CommandProperty, value);
+        }
+
+        public static void SetCommandParameter(DependencyObject target, object value)
+        {
+            target.SetValue(CommandParameterProperty, value);
+        }
+        public static object GetCommandParameter(DependencyObject target)
+        {
+            return target.GetValue(CommandParameterProperty);
+        }
+
+        private static void CommandChanged(DependencyObject target, DependencyPropertyChangedEventArgs e)
+        {
+            Control control = target as Control;
+            if (control != null)
+            {
+                if ((e.NewValue != null) && (e.OldValue == null))
+                {
+                    control.MouseDown += OnMouseDown;
+                }
+                else if ((e.NewValue == null) && (e.OldValue != null))
+                {
+                    control.MouseDoubleClick -= OnMouseDown;
+                }
+            }
+        }
+
+        private static void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Control control = sender as Control;
+            ICommand command = (ICommand)control.GetValue(CommandProperty);
+            object commandParameter = control.GetValue(CommandParameterProperty);
+
+            if (e.ChangedButton == MouseButton.Middle && e.ButtonState == MouseButtonState.Pressed)
+                command.Execute(commandParameter);
+        }
+    }
+}
