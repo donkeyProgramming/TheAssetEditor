6f435f11fa2525033c7ecbe16d2f94e092915ef0
ole kristian homelien <ole_kristian92@hotmail.com>
2022-03-30 13:20:59 +0200

    TextureProject wip

--- KitbasherEditor/ViewModels/SceneExplorerNodeViews/TextureFileEditorServiceViewModel.cs
+++ KitbasherEditor/ViewModels/SceneExplorerNodeViews/TextureFileEditorServiceViewModel.cs
@@ -26,26 +26,12 @@ namespace KitbasherEditor.ViewModels.SceneExplorerNodeViews
     {
         ILogger _logger = Logging.Create<TextureFileEditorServiceViewModel>();
 
-        public class TextureItem
-        {
-            public bool PartOfProject { get; set; }
-            public string SystemFilePath { get; set; }
-            public bool IsFoundInProjectFolder { get; set; }
-     
-            public string PackFilePath { get; set; }
-            public string UpdatedPackFilePath { get; set; }
-
-            public TexureType Type { get; set; }
-            public bool HasErrors { get; set; }
-            public string ErrorString { get; set; } = "";
-        }
-
         MainEditableNode _mainNode;
         TextureFileEditorService _textureService;
         PackFileService _pfs;
 
         public ICommand CreateProjectCommand { get; set; }
-        public ICommand AddMissingTextureCommand { get; set; }
+        public ICommand RefreshProjectCommand { get; set; }
         public ICommand RefreshTexturesCommand { get; set; }
         public ICommand OpenFolderCommand { get; set; }
         public ICommand BrowseCommand { get; set; }
@@ -53,214 +39,32 @@ namespace KitbasherEditor.ViewModels.SceneExplorerNodeViews
 
         public NotifyAttr<bool> IsRunning { get; set; } = new NotifyAttr<bool>(false);
         public NotifyAttr<string> FilePath { get; set; } = new NotifyAttr<string>("");
-        public NotifyAttr<string> FilePrefix { get; set; } = new NotifyAttr<string>("");
+        public string FilePrefix { get => _textureService.FilePreFix; set => _textureService.FilePreFix = value; } 
 
-        public ObservableCollection<TextureItem> TextureList { get; set; } = new ObservableCollection<TextureItem>();
+        public ObservableCollection<TextureFileEditorService.TextureItem> TextureList { get; set; } = new ObservableCollection<TextureFileEditorService.TextureItem>();
 
         public TextureFileEditorServiceViewModel(MainEditableNode mainNode, PackFileService pfs)
         {
-            CreateProjectCommand = new RelayCommand(CreateProject);
-            AddMissingTextureCommand = new RelayCommand(AddMissingTextures);
-            RefreshTexturesCommand = new RelayCommand(RefreshTextures);
+            CreateProjectCommand = new RelayCommand(() => _textureService.CreateProject());
+            RefreshProjectCommand = new RelayCommand(() => _textureService.RefreshProject());
+            RefreshTexturesCommand = new RelayCommand(() => _textureService.RefreshTextures());
 
-            OpenFolderCommand = new RelayCommand(OpenFolder);
+            OpenFolderCommand = new RelayCommand(() => _textureService.OpenProjectFolder());
             BrowseCommand = new RelayCommand(Browse);
 
-            _pfs = pfs;
-            _mainNode = mainNode;
-            _textureService = _mainNode.TextureFileEditorService;
-
-            UpdateViewData();
+            _textureService = mainNode.TextureFileEditorService;
+            _textureService.UpdateStatus();
+            UpdateView();
         }
 
-        void UpdateViewData()
+
+        void UpdateView()
         {
-            var projectFolder = _textureService.ProjectPath;
-            if (string.IsNullOrEmpty(projectFolder))
-                projectFolder = DetermineDefaultProjectName(_mainNode);
-
-            string[] directoryContent = new string[0];
-            if(Directory.Exists(projectFolder))
-                directoryContent  = Directory.GetFiles(projectFolder);
-
-            var allTexutes = _mainNode.GetMeshesInLod(0, false)
-                   .SelectMany(x => x.Material.GetAllTextures())
-                   .DistinctBy(x => x.Path.ToLower())
-                   .ToList();
-
-            var textureList = new List<TextureItem>();
-            foreach (var texture in allTexutes)
-            {
-                var fileSystemName = DetermineTextureName(projectFolder, texture.Path);
-                var updateTexturePath = DetermineUpdatedTextureName(fileSystemName);
-                var isValidPath = _pfs.FindFile(texture.Path) != null;
-                var isValidTextureType = _textureService.ValidTextureTypes.Contains(texture.TexureType);
-                var isFoundInProjectFolder = directoryContent.Contains(fileSystemName, StringComparer.InvariantCultureIgnoreCase);
-
-                var errorStr = "";
-                if (!isValidPath)
-                    errorStr += "Unable to find texture. ";
-
-                if (!isValidTextureType)
-                    errorStr += "Texture type is not supported. ";
-
-                var textureItem = new TextureItem()
-                {
-                    IsFoundInProjectFolder = isFoundInProjectFolder,
-                    PackFilePath = texture.Path,
-                    SystemFilePath = DetermineTextureName(projectFolder, texture.Path),
-                    UpdatedPackFilePath = updateTexturePath,
-                    Type = texture.TexureType,
-                    PartOfProject = string.IsNullOrWhiteSpace(errorStr),
-                    HasErrors = string.IsNullOrWhiteSpace(errorStr),
-                    ErrorString = errorStr
-                };
-                textureList.Add(textureItem);
-            }
+            FilePath.Value = _textureService.ProjectPath;
 
             TextureList.Clear();
-            foreach (var item in textureList.OrderByDescending(x=>x.HasErrors))
-                TextureList.Add(item);
-
-            FilePrefix.Value = _textureService.FilePreFix;
-            FilePath.Value = projectFolder;
-        }
-
-
-        string DetermineTextureName(string outputFolder, string texturePath)
-        {
-            var prefix = _textureService.FilePreFix;
-            var textureName = Path.GetFileName(texturePath);
-            if (textureName.Contains(prefix, StringComparison.InvariantCultureIgnoreCase))
-                prefix = "";
-
-            var path = outputFolder + "\\" + prefix + textureName;
-            path = Path.ChangeExtension(path, "png");
-            return path;
-        }
-
-        string DetermineUpdatedTextureName(string fileSystemPath)
-        {
-            var filePath = fileSystemPath;
-            filePath = filePath.Replace(DirectoryHelper.Temp, "variantmeshes\\wh_variantmodels", StringComparison.InvariantCultureIgnoreCase);
-            filePath = Path.ChangeExtension(filePath, "dds");
-            return filePath;
-        }
-
-        string DetermineDefaultProjectName(MainEditableNode node)
-        {
-            var path = _pfs.GetFullPath(node.MainPackFile);
-            var dirName =  Path.GetDirectoryName(path);
-            dirName = dirName.Replace(@"variantmeshes\wh_variantmodels\", "", StringComparison.InvariantCultureIgnoreCase);
-            var projectDir = DirectoryHelper.Temp + "\\" + dirName;
-            return projectDir;
-        }
-
-        public void CreateProject()
-        {
-            // Ensure folder is created and empty! 
-            var projectPath = FilePath.Value;
-            _logger.Here().Information($"Creating project at {projectPath}");
-
-            if (Directory.Exists(projectPath))
-            {
-                var numFiles = Directory.GetFiles(projectPath).Length;
-                if (numFiles != 0)
-                {
-                    if (MessageBox.Show($"Project folder {projectPath} already exists. Do you want to delete it?", "Warning", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
-                    {
-                        try
-                        {
-                            Directory.Delete(projectPath, true);
-                        }
-                        catch (Exception e)
-                        {
-                            _logger.Here().Error($"Unable to delete folder {e.Message}");
-                            MessageBox.Show("Unable to delete folder - Can not createa a new project");
-                            return;
-                        }
-                    }
-                    else
-                    {
-                        return;
-                    }
-                }
-            }
-
-            DirectoryHelper.EnsureCreated(projectPath);
-            _textureService.ProjectPath = projectPath;
-
-            foreach (var texture in TextureList)
-            {
-                if (texture.PartOfProject)
-                {
-                    var folderPath = Path.GetDirectoryName(texture.SystemFilePath);
-                    DirectoryHelper.EnsureCreated(folderPath);
-                    ExportTextureToFile(texture); 
-                }
-            }
-
-            UpdateTexturesFromDirectory();
-            _textureService.SaveUvMaps(projectPath);
-            UpdateViewData();
-            OpenFolder();
-        }
-
-        void UpdateTexturesFromDirectory()
-        {
-            foreach (var texture in TextureList)
-            {
-                if (texture.PartOfProject)
-                {
-                    AddTextureToPackFile(texture);
-
-                    // Determine which models use this texture and update them
-                    var allMeshes = _mainNode.GetMeshesInLod(0, false);
-                    foreach (var model in allMeshes)
-                    {
-                        // To array to create a copy of the list, so we dont get an exception while changing the list while itterating
-                        var modelTextures = model.Material.GetAllTextures().ToArray();
-                        foreach (var modelTexture in modelTextures)
-                        {
-                            var equalType = modelTexture.TexureType == texture.Type;
-                            var equalPath = string.Compare(modelTexture.Path, texture.PackFilePath, StringComparison.InvariantCultureIgnoreCase) == 0;
-                            if (equalType && equalPath)
-                                model.UpdateTexture(texture.UpdatedPackFilePath, modelTexture.TexureType, true);
-                        }
-                    }
-                }
-            }
-        }
-
-
-        private bool ExportTextureToFile(TextureItem texture)
-        {
-            var packFile = _pfs.FindFile(texture.PackFilePath);
-            return TextureConverter.SaveAsPNG(packFile, texture.SystemFilePath);
-        }
-
-
-        private PackFile AddTextureToPackFile(TextureItem texture)
-        {
-            var filePath = texture.SystemFilePath;
-            filePath = filePath.Replace(DirectoryHelper.Temp, "variantmeshes\\wh_variantmodels", StringComparison.InvariantCultureIgnoreCase);
-            var packFile = TextureConverter.LoadTexture(_pfs, filePath, texture.SystemFilePath, texture.Type);
-            return packFile;
-        }
-
-
-
-        public void AddMissingTextures()
-        {
-    
-        }
-
-        void OpenFolder()
-        {
-            if (Directory.Exists(FilePath.Value))
-                Process.Start("explorer.exe", FilePath.Value);
-            else
-                MessageBox.Show("Project folder not found");
+            foreach (var textureItem in _textureService.TextureList)
+                TextureList.Add(textureItem);
         }
 
         void Browse()
@@ -271,30 +75,8 @@ namespace KitbasherEditor.ViewModels.SceneExplorerNodeViews
             if (result != DialogResult.OK)
                 return;
 
-            FilePath.Value = dialog.SelectedPath;
-            _textureService.ProjectPath = dialog.SelectedPath;
-            UpdateViewData();
-        }
-
-        void RefreshTextures()
-        {
-            UpdateTexturesFromDirectory();
-        }
-
-        string CreateDefaultOutputDirectoryPath(MainEditableNode mainNode)
-        {
-            var path = DirectoryHelper.Temp + "\\" + Path.GetFileNameWithoutExtension(mainNode.MainPackFile.Name);
-            for (int index = 0; index < 1024; index++)
-            {
-                var potentialPath = (index == 0) ? path : string.Format("{0} _{1}", path, index);
-                if (Directory.Exists(potentialPath))
-                    continue;
-
-                path = potentialPath;
-                break;
-            }
-
-            return path;
+            _textureService.SetProjectDirectory(dialog.SelectedPath);
+            UpdateView();
         }
     }
 }
--- View3D/Services/TextureFileEditorService.cs
+++ View3D/Services/TextureFileEditorService.cs
@@ -1,11 +1,18 @@
 ﻿using CommonControls.Common;
+using CommonControls.FileTypes.PackFiles.Models;
 using CommonControls.FileTypes.RigidModel.Types;
 using CommonControls.Services;
+using MoreLinq;
+using Serilog;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Windows;
 using View3D.SceneNodes;
 using View3D.Utility;
@@ -14,24 +21,39 @@ namespace View3D.Services
 {
     public class TextureFileEditorService
     {
+        ILogger _logger = Logging.Create<TextureFileEditorService>();
         public string FilePreFix { get; set; } = "cust_";
         public string ProjectPath { get; set; } = "";
-
+        readonly string ProjectFileName = "TextureProject.Json";
         public class Texture
         {
             public TexureType Type { get; set; }
             public string GamePath { get; set; }
             public string SystemPath { get; set; }
-            public string Error { get; set; }
         }
 
-        public string OutputDirectory{ get; set; }
-       
+        public class TextureItem
+        {
+            public bool PartOfProject { get; set; }
+            public string SystemFilePath { get; set; }
+            public bool IsFoundInProjectFolder { get; set; }
+
+            public string PackFilePath { get; set; }
+            public string UpdatedPackFilePath { get; set; }
+
+            public TexureType Type { get; set; }
+            public bool HasErrors { get; set; }
+            public string ErrorString { get; set; } = "";
+        }
+
+        //public List<Texture> GetCurrentTextures() => _textures;
         public List<TexureType> ValidTextureTypes { get => new List<TexureType>() { TexureType.BaseColour, TexureType.Normal, TexureType.Mask, TexureType.MaterialMap }; }
+        public List<TextureItem> TextureList { get; set; } = new List<TextureItem>();
 
         List<Texture> _textures { get; set; } = new List<Texture>();
         PackFileService _pfs;
         MainEditableNode _node;
+
 
         public TextureFileEditorService(MainEditableNode node, PackFileService pfs)
         {
@@ -40,12 +62,306 @@ namespace View3D.Services
         }
 
 
-        public void CreateProject(string systemPath, string prefix, List<Texture> textures)
-        { 
-        
+        public void UpdateStatus()
+        {
+            TextureList.Clear();
+
+            // Check if there is a project file there
+            var loadResult = LoadProject(ProjectPath + "\\" + ProjectFileName, false);
+            if (loadResult == true)
+                return;
+
+            ProjectPath = DetermineDefaultProjectName(_node);
+            var allTexutes = _node.GetMeshesInLod(0, false)
+                   .SelectMany(x => x.Material.GetAllTextures())
+                   .DistinctBy(x => x.Path.ToLower())
+                   .ToList();
+
+            var cfgTextureItems = new List<Texture>();
+            foreach (var texture in allTexutes)
+            {
+                cfgTextureItems.Add(new Texture()
+                {
+                    Type = texture.TexureType,
+                    SystemPath = DetermineTextureName(ProjectPath, texture.Path),
+                    GamePath = texture.Path
+                });
+            }
+
+            BuildStatusFromCfg(cfgTextureItems);
         }
 
-        public List<Texture> GetCurrentTextures() => _textures;
+        void BuildStatusFromCfg(List<Texture> cfgItems)
+        {
+            string[] directoryContent = new string[0];
+            if (Directory.Exists(ProjectPath))
+                directoryContent = Directory.GetFiles(ProjectPath);
+
+            foreach (var texture in cfgItems)
+            {
+                var fileSystemName = texture.SystemPath;
+                var updateTexturePath = DetermineUpdatedTextureName(fileSystemName);
+                var isValidPath = _pfs.FindFile(texture.GamePath) != null;
+                var isValidTextureType = ValidTextureTypes.Contains(texture.Type);
+                var isFoundInProjectFolder = directoryContent.Contains(fileSystemName, StringComparer.InvariantCultureIgnoreCase);
+
+                var errorStr = "";
+                if (!isValidPath)
+                    errorStr += "Unable to find texture.";
+
+                if (!isValidTextureType)
+                    errorStr += "Texture type is not supported.";
+
+                var textureItem = new TextureItem()
+                {
+                    Type = texture.Type,
+                    SystemFilePath = texture.SystemPath,
+                    PackFilePath = texture.GamePath,
+
+                    //UpdatedPackFilePath = updateTexturePath,
+
+                    IsFoundInProjectFolder = isFoundInProjectFolder,
+                    PartOfProject = string.IsNullOrWhiteSpace(errorStr),
+                    HasErrors = !string.IsNullOrWhiteSpace(errorStr),
+                    ErrorString = errorStr
+                };
+                TextureList.Add(textureItem);
+            }
+
+            TextureList = TextureList.OrderBy(x => x.HasErrors).ToList();
+        }
+
+
+        bool EnsureCleanProject(string projectPath)
+        {
+            if (Directory.Exists(projectPath))
+            {
+                var numFiles = Directory.GetFiles(projectPath).Length;
+                if (numFiles != 0)
+                {
+                    if (MessageBox.Show($"Project folder {projectPath} already exists. Do you want to delete it?", "Warning", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    {
+                        try
+                        {
+                            _logger.Here().Information($"Deleting project at {projectPath}");
+                            Directory.Delete(projectPath, true);
+                        }
+                        catch (Exception e)
+                        {
+                            _logger.Here().Error($"Unable to delete folder {e.Message}");
+                            MessageBox.Show("Unable to delete folder - Can not createa a new project");
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            DirectoryHelper.EnsureCreated(projectPath);
+            _logger.Here().Information($"New project at {projectPath}");
+            return true;
+        }
+
+        public void CreateProject()
+        {
+            _logger.Here().Information($"Creating project at {ProjectPath}");
+
+            // Ensure folder is created and empty! Clean up the project if there is already one there. Return if the user dont want to delete it
+            if (EnsureCleanProject(ProjectPath) == false)
+                return;
+
+            var projectCfg = new List<Texture>();
+            foreach (var texture in TextureList)
+            {
+                if (texture.PartOfProject)
+                {
+                    var fileSystemName = texture.SystemFilePath;
+                    var updateTexturePath = DetermineUpdatedTextureName(fileSystemName);
+
+                    projectCfg.Add(new Texture()
+                    {
+                        Type = texture.Type,
+                        GamePath = updateTexturePath,
+                        SystemPath = texture.SystemFilePath,
+                    });
+
+                    var folderPath = Path.GetDirectoryName(texture.SystemFilePath);
+                    DirectoryHelper.EnsureCreated(folderPath);
+                    ExportTextureToFile(texture);
+                    AddTextureToPackFile(texture);
+
+                    // Upate reference in meshes
+                    // Determine which models use this texture and update them
+                    var allMeshes = _node.GetMeshesInLod(0, false);
+                    foreach (var model in allMeshes)
+                    {
+                        // To array to create a copy of the list, so we dont get an exception while changing the list while itterating
+                        var modelTextures = model.Material.GetAllTextures().ToArray();
+                        foreach (var modelTexture in modelTextures)
+                        {
+                            var equalType = modelTexture.TexureType == texture.Type;
+                            var equalPath = string.Compare(modelTexture.Path, texture.PackFilePath, StringComparison.InvariantCultureIgnoreCase) == 0;
+
+                            if (equalType && equalPath)
+                                model.UpdateTexture(updateTexturePath, modelTexture.TexureType, true);
+                        }
+                    }
+                }
+            }
+
+            var projectCfgStr = JsonSerializer.Serialize(projectCfg, GetJsonConverterOptions());
+            File.WriteAllText(ProjectPath + "\\" + ProjectFileName, projectCfgStr);
+            SaveUvMaps(ProjectPath);
+            UpdateStatus();
+            OpenProjectFolder();
+            _logger.Here().Information($"Texture project created");
+        }
+
+        public void RefreshProject()
+        {
+            // Are you sure
+            // Read cfg and update paths (game path key)
+            // add missing textures to cfg and export them
+            // Remove textures no longer part of project
+        }
+
+
+        public void UpdateProject()
+        {
+            // Update settings
+            // Update project files
+            // Export missing files - textures and uvs
+        }
+
+        public void SetProjectDirectory(string newPath)
+        {
+            ProjectPath = newPath;
+        }
+
+
+        public bool LoadProject(string projectFilePath, bool updateTextures)
+        {
+            if (File.Exists(projectFilePath) == false)
+                return false;
+
+            try
+            {
+                var bytes = File.ReadAllBytes(projectFilePath);
+                var projectCfg = JsonSerializer.Deserialize<List<Texture>>(bytes, GetJsonConverterOptions());
+                BuildStatusFromCfg(projectCfg);
+            }
+            catch (Exception e)
+            {
+                _logger.Here().Error($"Unable to load Project at {projectFilePath} error: {e.Message}");
+                MessageBox.Show($"Unable to load project - {e.Message}");
+                return false;
+            }
+
+            if (updateTextures)
+                RefreshTextures();
+
+            return true;
+        }
+
+        public void RefreshTextures()
+        {
+            foreach (var texture in TextureList)
+            {
+                if (texture.PartOfProject)
+                {
+                    AddTextureToPackFile(texture);
+            
+                    // Determine which models use this texture and update them
+                    var allMeshes = _node.GetMeshesInLod(0, false);
+                    foreach (var model in allMeshes)
+                    {
+                        // To array to create a copy of the list, so we dont get an exception while changing the list while itterating
+                        var modelTextures = model.Material.GetAllTextures().ToArray();
+                        foreach (var modelTexture in modelTextures)
+                        {
+                            var equalType = modelTexture.TexureType == texture.Type;
+                            var equalPath = string.Compare(modelTexture.Path, texture.PackFilePath, StringComparison.InvariantCultureIgnoreCase) == 0;
+                            if (equalType && equalPath)
+                                model.UpdateTexture(texture.PackFilePath, modelTexture.TexureType, true);
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public void OpenProjectFolder()
+        {
+            if (Directory.Exists(ProjectPath))
+                Process.Start("explorer.exe", ProjectPath);
+            else
+                MessageBox.Show("Project folder not found");
+        }
+
+
+        JsonSerializerOptions GetJsonConverterOptions()
+        {
+            var jsonOptions = new JsonSerializerOptions() { WriteIndented = true };
+            jsonOptions.Converters.Add(new JsonStringEnumConverter());
+            return jsonOptions;
+        }
+
+        private bool ExportTextureToFile(TextureItem texture)
+        {
+            var packFile = _pfs.FindFile(texture.PackFilePath);
+            return TextureConverter.SaveAsPNG(packFile, texture.SystemFilePath);
+        }
+
+
+        private PackFile AddTextureToPackFile(TextureItem texture)
+        {
+            var filePath = texture.SystemFilePath;
+            filePath = filePath.Replace(DirectoryHelper.Temp, "variantmeshes\\wh_variantmodels", StringComparison.InvariantCultureIgnoreCase);
+            var packFile = TextureConverter.LoadTexture(_pfs, filePath, texture.SystemFilePath, texture.Type);
+            return packFile;
+        }
+
+        //---------------------------
+
+        string DetermineTextureName(string outputFolder, string texturePath)
+        {
+            var prefix = FilePreFix;
+            var textureName = Path.GetFileName(texturePath);
+            if (textureName.Contains(prefix, StringComparison.InvariantCultureIgnoreCase))
+                prefix = "";
+
+            var path = outputFolder + "\\" + prefix + textureName;
+            path = Path.ChangeExtension(path, "png");
+            return path;
+        }
+
+        string DetermineUpdatedTextureName(string fileSystemPath)
+        {
+            var filePath = fileSystemPath;
+            filePath = filePath.Replace(DirectoryHelper.Temp, "variantmeshes\\wh_variantmodels", StringComparison.InvariantCultureIgnoreCase);
+            filePath = Path.ChangeExtension(filePath, "dds");
+            return filePath;
+        }
+
+        string DetermineDefaultProjectName(MainEditableNode node)
+        {
+            if (string.IsNullOrWhiteSpace(ProjectPath) == false)
+                return ProjectPath;
+
+            var path = _pfs.GetFullPath(node.MainPackFile);
+            var dirName = Path.GetDirectoryName(path);
+            dirName = dirName.Replace(@"variantmeshes\wh_variantmodels\", "", StringComparison.InvariantCultureIgnoreCase);
+            var projectDir = DirectoryHelper.Temp + "\\" + dirName;
+            return projectDir;
+        }
+
+
+
+
+        //---------------------------
+
 
         public void SaveUvMaps(string projectPath)
         {
