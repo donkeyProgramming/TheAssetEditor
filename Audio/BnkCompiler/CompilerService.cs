9247d8457ef4fc010a84c3e22936712f6125fa22
ole kristian homelien <ole_kristian92@hotmail.com>
2023-04-24 21:52:28 +0200

    Cleanup

--- Audio/BnkCompiler/CompilerService.cs
+++ Audio/BnkCompiler/CompilerService.cs
@@ -1,24 +1,8 @@
 ﻿using CommonControls.Common;
 using Serilog;
-using static CommonControls.BaseDialogs.ErrorListDialog.ErrorListViewModel;
 
 namespace Audio.BnkCompiler
 {
-    public interface ICompilerLogger
-    {
-        public void Log(ErrorList errorList);
-    }
-
-    public class CompilerConsoleLogger : ICompilerLogger
-    {
-        ILogger _logger = Logging.Create<CompilerConsoleLogger>();
-        public void Log(ErrorList errorList)
-        {
-            foreach (var item in errorList.Errors)
-                _logger.Error(item.PrettyString);
-        }
-    }
-
     public class CompilerSettings
     {
         public bool UserOerrideIdForActions { get; set; } = false;
