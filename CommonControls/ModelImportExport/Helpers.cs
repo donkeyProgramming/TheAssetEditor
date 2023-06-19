271cbeb3d46672b713e45e7bc58d933b54705eae
mr-phazer <phazer1980@msn.com>
2023-06-19 21:11:05 +0200

    Edits 4

---
+++ CommonControls/ModelImportExport/Helpers.cs
@@ -0,0 +1,12 @@
+﻿using Assimp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonControls.ModelImportExport
+{
+    public class Helpers
+    {
+       
+    }
+}
