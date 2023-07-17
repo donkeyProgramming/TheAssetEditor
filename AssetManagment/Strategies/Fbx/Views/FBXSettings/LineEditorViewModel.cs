ecb18acd4019ae8a3970365e1b4f1aa57b0c108d
mr-phazer <phazer1980@msn.com>
2023-07-17 15:17:07 +0200

    Exmperiment with IMarhaller

---
+++ AssetManagment/Strategies/Fbx/Views/FBXSettings/LineEditorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommonControls.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Strategies.Fbx.Views.FBXSettings
+{
+    class LineEditorViewModel
+    {
+        public NotifyAttr<string> LabelControl { get; set; } = new NotifyAttr<string>($"empty.fbx");
+        public NotifyAttr<string> TextBoxControl { get; set; } = new NotifyAttr<string>($"Inches");
+
+
+    }
+}
