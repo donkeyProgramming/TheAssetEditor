a8e8625b624035f9997e58113787e0cae59e2807
mr-phazer <phazer1980@msn.com>
2023-07-19 22:41:03 +0200

    Resolve Conflict.

--- AssetManagment/Strategies/Fbx/SceneLoader.cs
+++ AssetManagment/Strategies/Fbx/SceneLoader.cs
@@ -1,16 +1,11 @@
 ﻿using AssetManagement.GenericFormats;
 using AssetManagement.Strategies.Fbx.DllDefinitions;
-using AssetManagement.Strategies.Fbx.Models;
-using AssetManagement.Strategies.Fbx.ViewModels;
-using CommonControls.Common;
-using Serilog;
 using System;
 
 namespace AssetManagement.Strategies.Fbx
 {
     public class SceneLoader
     {
-        static private ILogger _logger = Logging.Create<SceneLoader>();
 
         public static SceneContainer LoadScene(string fileName)
         {
