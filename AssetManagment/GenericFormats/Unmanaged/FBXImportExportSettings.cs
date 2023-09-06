16fdd7342763ca1bb1c6888d17955e325ed86a40
mr-phazer <phazer1980@msn.com>
2023-09-06 15:53:29 +0200

    Pr-build Again

---
+++ AssetManagment/GenericFormats/Unmanaged/FBXImportExportSettings.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AssetManagement.GenericFormats.Unmanaged
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FBXSDKVersion
+    {
+        public int x;
+        public int y;
+        public int z;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FBXFileInfo
+    {
+        public FBXSDKVersion sdkVersion; // SDK version file is saved with;
+        public bool isASCII; // FBX can be saves as binary or json/xml like ASCII text
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FBXUnitFileInfo
+    {
+        public string unitNameString;
+        public float globalScale;
+        public float SsaleToMeters;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FBXAnimInfo
+    {
+        public string skeletonName;
+        public float animFPS;
+        public float animTiem;
+        public int animFrame;
+    }        
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FBXImportExportSettings    
+    {     
+        public FBXFileInfo fileinfo;
+        public FBXUnitFileInfo unitInfo;
+        public FBXAnimInfo animTimingINfo;
+    }
+}
---
+++ AssetManagment/Strategies/Fbx/Views/FBXSettings/FbxSettingsView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+
+namespace AssetManagement.Strategies.Fbx.Views.FBXSettings
+{
+    /// <summary>
+    /// Interaction logic for d.xaml
+    /// </summary>
+    public partial class FBXSetttingsView : Window
+    {
+        public FBXSetttingsView()
+        {
+            InitializeComponent();
+
+            ImportButton.Click += ImportButton_Click;
+        }
+
+        private void ImportButton_Click(object sender, RoutedEventArgs e)     
+        {
+            
+            DialogResult = true;
+            Close();
+        }
+
+    }
+}
