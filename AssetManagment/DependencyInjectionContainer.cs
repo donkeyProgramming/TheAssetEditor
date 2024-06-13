722748fbcd8eb39436d301a44d781ab99c73a712
ole kristian homelien <ole_kristian92@hotmail.com>
2024-06-13 12:36:12 +0200

    Rebaned editor

--- AssetManagment/DependencyInjectionContainer.cs
+++ AssetManagment/DependencyInjectionContainer.cs
@@ -4,9 +4,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Shared.Core.DependencyInjection;
 using Shared.Ui.Interfaces.AssetManagement;
 
-namespace AssetManagement
+namespace Editors.AssetManagement
 {
-    public class AssetManagement_DependencyInjectionContainer : DependencyContainer
+    public class DependencyInjectionContainer : DependencyContainer
     {
         public override void Register(IServiceCollection serviceCollection)
         {
