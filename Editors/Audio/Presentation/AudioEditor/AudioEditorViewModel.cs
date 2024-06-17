80160ae3f48b15bd8dd383391cb0a45bc398ba6a
Pear <61670316+Pear-231@users.noreply.github.com>
2024-06-17 12:59:18 +0100

    Change Notes: - Basic form of AudioEditor. - Having some issue where adding new rows deletes the data in the previous row and also data doesn't seem to be recorded in the DataGridItems when there's only one row.

--- Editors/Audio/Presentation/AudioEditor/AudioEditorViewModel.cs
+++ Editors/Audio/Presentation/AudioEditor/AudioEditorViewModel.cs
@@ -1,4 +1,12 @@
-﻿using Shared.Core.Misc;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using Editors.Audio.Presentation.AudioExplorer;
+using Editors.Audio.Storage;
+using Shared.Core.Misc;
 using Shared.Core.PackFiles;
 using Shared.Core.PackFiles.Models;
 using Shared.Core.ToolCreation;
@@ -7,25 +15,84 @@ namespace Editors.Audio.Presentation.AudioEditor
 {
     public class AudioEditorViewModel : NotifyPropertyChangedImpl, IEditorViewModel
     {
-        private readonly PackFileService _pfs;
-
         public NotifyAttr<string> DisplayName { get; set; } = new NotifyAttr<string>("Audio Editor");
-        public NotifyAttr<int> ExampleVariable { get; set; } = new NotifyAttr<int>(3);
 
-        public AudioEditorViewModel(PackFileService pfs)
+        private readonly PackFileService _packFileService;
+        private readonly IAudioRepository _audioRepository;
+        private string _selectedEventName;
+
+        public ICommand AddEventCommand { get; set; }
+        public ICommand AddStatePathCommand { get; set; }
+
+        public EventSelectionFilter EventFilter { get; set; }
+
+        public ObservableCollection<Dictionary<string, string>> DataGridItems { get; set; } = new ObservableCollection<Dictionary<string, string>>();
+
+        public AudioEditorViewModel(PackFileService packFileService, IAudioRepository audioRepository)
         {
-            _pfs = pfs;
+            _packFileService = packFileService;
+            _audioRepository = audioRepository;
+
+            AddEventCommand = new RelayCommand(AddEvent);
+            AddStatePathCommand = new RelayCommand(AddStatePath);
+
+            EventFilter = new EventSelectionFilter(_audioRepository, false, true);
+            EventFilter.EventList.SelectedItemChanged += OnEventSelected;
         }
 
-        public void ExampleAction()
+        public void Close()
         {
-            ExampleVariable.Value++;
-            HasUnsavedChanges = true;
+            EventFilter.EventList.SelectedItemChanged -= OnEventSelected;
         }
 
-        public void Close() { }
-        public bool Save() => true;
+        public bool Save()
+        {
+            return true;
+        }
+
         public PackFile MainFile { get; set; }
         public bool HasUnsavedChanges { get; set; } = false;
+
+        private void OnEventSelected(SelectedHircItem newValue)
+        {
+            if (newValue == null)
+                return;
+
+            _selectedEventName = newValue.DisplayName.ToString();
+            Debug.WriteLine($"selectedEventName: {_selectedEventName}");
+        }
+
+        private void AddEvent()
+        {
+            if (_selectedEventName == null)
+                return;
+
+            var dataGrid = AudioEditorViewModelHelpers.GetDataGrid();
+            dataGrid.Columns.Clear();
+            DataGridItems.Clear();
+
+            var dialogueEventsWithStateGroups = _audioRepository.DialogueEventsWithStateGroups;
+            var stateGroups = dialogueEventsWithStateGroups[_selectedEventName];
+
+            foreach (var stateGroup in stateGroups)
+            {
+                var column = new DataGridTemplateColumn
+                {
+                    Header = AudioEditorViewModelHelpers.AddExtraUnderScoresToStateGroup(stateGroup),
+                    CellTemplate = AudioEditorViewModelHelpers.CreateComboBoxTemplate(_audioRepository, stateGroup)
+                };
+
+                dataGrid.Columns.Add(column);
+            }
+        }
+
+        private void AddStatePath()
+        {
+            if (_selectedEventName == null)
+                return;
+
+            var newRow = new Dictionary<string, string>();
+            DataGridItems.Add(newRow);
+        }
     }
 }
---
+++ Editors/Audio/Presentation/AudioEditor/AudioEditorViewModelHelpers.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using Editors.Audio.Storage;
+using Newtonsoft.Json;
+
+
+namespace Editors.Audio.Presentation.AudioEditor
+{
+    public class AudioEditorViewModelHelpers
+    {
+        public static DataGrid GetDataGrid()
+        {
+            var mainWindow = Application.Current.MainWindow;
+            var dataGrid = FindVisualChild<DataGrid>(mainWindow, "AudioEditorDataGrid");
+            return dataGrid;
+        }
+
+        public static T FindVisualChild<T>(DependencyObject parent, string name) where T : DependencyObject
+        {
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                if (child is T typedChild && child is FrameworkElement element && element.Name == name)
+                {
+                    return typedChild;
+                }
+                else
+                {
+                    var foundChild = FindVisualChild<T>(child, name);
+                    if (foundChild != null)
+                        return foundChild;
+                }
+            }
+
+            return null;
+        }
+
+        public static DataTemplate CreateComboBoxTemplate(IAudioRepository audioRepository, string stateGroup)
+        {
+            var template = new DataTemplate();
+            var factory = new FrameworkElementFactory(typeof(ComboBox));
+
+            var binding = new Binding($"[{stateGroup}]")
+            {
+                Mode = BindingMode.TwoWay,
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+            };
+
+            factory.SetBinding(ComboBox.ItemsSourceProperty, new Binding { Source = audioRepository.StateGroupsWithStates[stateGroup] });
+            factory.SetBinding(ComboBox.SelectedItemProperty, binding);
+
+            template.VisualTree = factory;
+            return template;
+        }
+
+        public static string AddExtraUnderScoresToStateGroup(string stateGroup)
+        {
+            return stateGroup.Replace("_", "__"); // Apparently WPF doesn't_like_underscores
+        }
+
+        public static string SerializeDataGrid(ObservableCollection<Dictionary<string, string>> dataGridItems)
+        {
+            var rows = new List<Dictionary<string, string>>();
+
+            foreach (var item in dataGridItems)
+                rows.Add(item);
+
+            return JsonConvert.SerializeObject(rows, Formatting.Indented);
+        }
+    }
+}
