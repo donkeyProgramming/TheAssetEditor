7364a1e5e1628051d31cb9825d68623f5a449b11
ole kristian homelien <ole_kristian92@hotmail.com>
2024-06-30 20:49:48 +0200

    Code

---
+++ Testing/E2EVerification/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AssetEditor.Services;
+using KitbasherEditor.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace E2EVerification
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var _serviceProvider = new DependencyInjectionConfig().Build();
+            var _rootScope = _serviceProvider.CreateScope();
+
+
+            var kitbasher = _rootScope.ServiceProvider.GetRequiredService<KitbasherViewModel>();
+            //kitbasher.MainFile 
+        }
+    }
+}
