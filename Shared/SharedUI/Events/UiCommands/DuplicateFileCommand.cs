e975b65e02a3445679ec5f309cdefc3f8b5e9995
Josh Ewart <134755835+HorseF3thers@users.noreply.github.com>
2024-07-15 12:18:06 -0500

    Fixes for duplicate command test cases
    Fixed an issue with the packfile saving, also fixed an issue with a ArgumentOutOfBounds exception when substring would be run on a file without extension. I chose to make an extra helper method to clean up what is occurring in the execute function as well.

--- Shared/SharedUI/Events/UiCommands/DuplicateFileCommand.cs
+++ Shared/SharedUI/Events/UiCommands/DuplicateFileCommand.cs
@@ -1,6 +1,7 @@
 ﻿using Shared.Core.Events;
 using Shared.Core.PackFiles;
 using Shared.Core.PackFiles.Models;
+using System;
 using System.IO;
 
 namespace Shared.Ui.Events.UiCommands
@@ -16,17 +17,34 @@ namespace Shared.Ui.Events.UiCommands
 
         public void Execute(PackFile item)
         {
-            var fileName = item.Name.Substring(0, item.Name.IndexOf("."));
-            var extension = item.Name.Substring(item.Name.IndexOf("."));
+            try
+            {
+                var fileName = item.Name.Substring(0, item.Name.IndexOf("."));
+                var extension = item.Name.Substring(item.Name.IndexOf("."));
+                var newName = fileName + "_copy" + extension;
 
-            var newName = fileName + "_copy" + extension;
+                readAndSave(fileName, newName, item);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                var fileName = item.Name;
+                var newName = fileName + "_copy";
+
+                readAndSave(fileName, newName, item);
+            }
+        }
+
+        public void readAndSave(string filename, string newName, PackFile item)
+        {
             var bytes = item.DataSource.ReadData();
             var packFile = new PackFile(newName, new MemorySource(bytes));
             var parentPath = _packFileService.GetFullPath(item);
             var packContainer = _packFileService.GetPackFileContainer(item);
             var path = Path.GetDirectoryName(parentPath);
 
-            _packFileService.AddFileToPack(packContainer, path, packFile);
+            _packFileService.AddFileToPack(_packFileService.GetEditablePack(), path, packFile);
         }
+
+
     }
 }
