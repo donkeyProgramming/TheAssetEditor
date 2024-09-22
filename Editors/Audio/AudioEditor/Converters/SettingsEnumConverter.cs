86582bfd6174f3d7a545bf3c4ee07c05a7b3c9ab
Pear <61670316+Pear-231@users.noreply.github.com>
2024-09-22 17:41:16 +0100

    Change Notes: - Reorganised and improved converters.

---
+++ Editors/Audio/AudioEditor/Converters/SettingsEnumConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using static Editors.Audio.AudioEditor.AudioEditorSettings;
+
+namespace Editors.Audio.AudioEditor.Converters
+{
+    public class SettingsEnumConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return value;
+
+            else if (value is Language language)
+                return GetStringFromLanguage(language);
+
+            else if (value is AudioType EventType)
+                return GetStringFromAudioType(EventType);
+
+            else if (value is AudioSubtype dialogueEventSubtype)
+                return GetStringFromAudioSubtype(dialogueEventSubtype);
+
+            else
+                return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
