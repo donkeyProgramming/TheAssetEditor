7bc0b85d26b3d7ae433e730dcc18fb61af4c2550
Pear <61670316+Pear-231@users.noreply.github.com>
2024-09-22 17:40:13 +0100

    Change Notes: - Updated View according to the new ViewModel.

--- Editors/Audio/AudioEditor/Views/AudioEditorView.xaml.cs
+++ Editors/Audio/AudioEditor/Views/AudioEditorView.xaml.cs
@@ -1,15 +1,50 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Editors.Audio.AudioEditor.ViewModels;
 
 namespace Editors.Audio.AudioEditor
 {
-    /// <summary>
-    /// Interaction logic for AudioEditorView.xaml
-    /// </summary>
     public partial class AudioEditorView : UserControl
     {
+        public AudioEditorViewModel ViewModel => DataContext as AudioEditorViewModel;
+
         public AudioEditorView()
         {
             InitializeComponent();
+
+            AudioProjectViewerDataGrid.SelectionChanged += DataGrid_SelectionChanged;
+        }
+
+        
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var dataGrid = sender as DataGrid;
+            var selectedItems = dataGrid.SelectedItems;
+
+            ViewModel.IsPlayAudioButtonEnabled = false;
+
+            if (ViewModel != null && selectedItems != null)
+            {
+                var selectedDataGridRows = ViewModel.SelectedDataGridRows;
+                selectedDataGridRows.Clear();
+
+                foreach (var item in selectedItems.OfType<Dictionary<string, object>>())
+                    selectedDataGridRows.Add(item);
+
+                if (selectedDataGridRows.Count == 1)
+                {
+                    if (selectedDataGridRows[0].TryGetValue("AudioFiles", out var audioFilesObj) && audioFilesObj is List<string> audioFiles && audioFiles.Any())
+                        ViewModel.IsPlayAudioButtonEnabled = true;
+                }
+            }
+        }
+
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (e.NewValue != null)
+                ViewModel.OnSelectedAudioProjectEventChanged(e.NewValue);
         }
     }
 }
