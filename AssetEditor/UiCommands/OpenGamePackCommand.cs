1506f9b165932a7aafe500feeff9e21e092397e0
Pear <61670316+Pear-231@users.noreply.github.com>
2024-10-06 00:51:44 +0100

    Change Notes: - Added a check to prevent the loading of game Pack files already loaded.

--- AssetEditor/UiCommands/OpenGamePackCommand.cs
+++ AssetEditor/UiCommands/OpenGamePackCommand.cs
@@ -1,9 +1,9 @@
 ﻿using System.Linq;
+using System.Windows.Forms;
 using Shared.Core.Events;
 using Shared.Core.PackFiles;
 using Shared.Core.Services;
 using Shared.Ui.Common;
-
 namespace AssetEditor.UiCommands
 {
     internal class OpenGamePackCommand : IUiCommand
@@ -24,11 +24,22 @@ namespace AssetEditor.UiCommands
             var settingsService = _applicationSettingsService;
             var settings = settingsService.CurrentSettings;
             var gamePath = settings.GameDirectories.FirstOrDefault(x => x.Game == game);
+
             if (gamePath == null || string.IsNullOrWhiteSpace(gamePath.Path))
             {
                 System.Windows.MessageBox.Show("No path provided for game");
                 return;
             }
+
+            foreach (var packFile in _packFileService.Database.PackFiles)
+            {
+                if (packFile.SystemFilePath == gamePath.Path)
+                {
+                    MessageBox.Show($"Pack files for \"{_gameInformationFactory.GetGameById(game).DisplayName}\" are already loaded.", "Error");
+                    return;
+                }
+            }
+
             using (new WaitCursor())
             {
                 _packFileService.LoadAllCaFiles(gamePath.Path, _gameInformationFactory.GetGameById(game).DisplayName);
