db0d6fb1614f427143a48265b6f9db400238895c
ole kristian homelien <ole_kristian92@hotmail.com>
2024-10-27 10:36:28 +0100

    Fixing removed code

--- Editors/AnimationMeta/SuperView/SuperViewViewModel.cs
+++ Editors/AnimationMeta/SuperView/SuperViewViewModel.cs
@@ -7,6 +7,7 @@ using Editors.Shared.Core.Common.BaseControl;
 using Editors.Shared.Core.Common.ReferenceModel;
 using Editors.Shared.Core.Services;
 using Microsoft.Xna.Framework;
+using Shared.Core.Events;
 using Shared.Core.Misc;
 using Shared.Core.PackFiles;
 using Shared.Core.PackFiles.Models;
@@ -18,8 +19,8 @@ namespace Editors.AnimationMeta.SuperView
         SceneObjectViewModel _asset;
 
         private readonly SceneObjectEditor _sceneObjectBuilder;
-        private readonly CopyPasteManager _copyPasteManager;
         private readonly SkeletonAnimationLookUpHelper _skeletonHelper;
+        private readonly IUiCommandFactory _uiCommandFactory;
         private readonly PackFileService _packFileService;
 
         public NotifyAttr<string> PersistentMetaFilePath { get; set; } = new NotifyAttr<string>("");
@@ -31,35 +32,34 @@ namespace Editors.AnimationMeta.SuperView
         public override Type EditorViewModelType => typeof(EditorView);
 
         public SuperViewViewModel(
+            IUiCommandFactory uiCommandFactory,
             PackFileService packFileService,
             SceneObjectEditor sceneObjectBuilder,
-            CopyPasteManager copyPasteManager,
             SkeletonAnimationLookUpHelper skeletonHelper,
             IEditorHostParameters editorHostParameters)
             : base(editorHostParameters)
         {
             DisplayName = "Super view";
-
+            _uiCommandFactory = uiCommandFactory;
             _packFileService = packFileService;
             _sceneObjectBuilder = sceneObjectBuilder;
-            _copyPasteManager = copyPasteManager;
             _skeletonHelper = skeletonHelper;
         }
 
         protected override void Initialize(SceneObjectViewModelBuilder builder, IList<SceneObjectViewModel> sceneNodeList)
         {
-            //PersistentMetaEditor = new EditorViewModel(_packFileService, _copyPasteManager);
-            //PersistentMetaEditor.EditorSavedEvent += PersistentMetaEditor_EditorSavedEvent;
-            //
-            //MetaEditor = new EditorViewModel(_packFileService, _copyPasteManager);
+            PersistentMetaEditor = new MetaDataEditorViewModel(_uiCommandFactory);
+           // PersistentMetaEditor.EditorSavedEvent += PersistentMetaEditor_EditorSavedEvent;
+            
+            MetaEditor = new MetaDataEditorViewModel(_uiCommandFactory);
             //MetaEditor.EditorSavedEvent += MetaEditor_EditorSavedEvent;
-            //
-            //var assetViewModel = builder.CreateAsset(true, "Root", Color.Black,null, true);
-            //sceneNodeList.Add(assetViewModel);
-            //
-            //_asset = assetViewModel;
-            //_asset.Data.MetaDataChanged += UpdateMetaDataInfoFromAsset;
-            //UpdateMetaDataInfoFromAsset(_asset.Data);
+            
+            var assetViewModel = builder.CreateAsset(true, "Root", Color.Black,null, true);
+            sceneNodeList.Add(assetViewModel);
+            
+            _asset = assetViewModel;
+            _asset.Data.MetaDataChanged += UpdateMetaDataInfoFromAsset;
+            UpdateMetaDataInfoFromAsset(_asset.Data);
         }
 
         public void Load(AnimationToolInput debugDataToLoad)
