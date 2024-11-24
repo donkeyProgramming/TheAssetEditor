e21d62217a2cb6b5b15b021a1acad7317125e858
ole kristian homelien <ole_kristian92@hotmail.com>
2024-11-24 19:47:30 +0100

    Testing time

---
+++ Testing/Shared.Core.Test/PackFiles/PackFileServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using Shared.Core.Events;
+using Shared.Core.PackFiles;
+using Shared.Core.PackFiles.Models;
+
+namespace Shared.Core.Test.PackFiles
+{
+    internal class PackFileServiceTest
+    {
+
+
+        [Test]
+        public void AddContainer_CaPackFile()
+        {
+            // Arrenge
+            var eventHub = new Mock<IGlobalEventHub>();
+            var pfs = new PackFileService(eventHub.Object);
+            var container = new PackFileContainer("MyTest");
+            container.SystemFilePath = "SystemPath";
+            container.IsCaPackFile = true;
+
+            // Act
+            pfs.AddContainer(container);
+
+            // Assert
+
+        }
+
+        // public void AddContainer_CaPackNotSetFile()
+        // public void AddContainers_Duplicate()
+        // public void AddContainers_NotDuplicate()
+
+
+
+        //CreateNewPackFileContainer
+
+        // AddFilesToPack
+
+        // CopyFileFromOtherPackFile
+
+        // SetEditablePack
+
+        // UnloadPackContainer
+
+        // SaveFile
+
+        // SavePackContainer
+    }
+}
