b2e334c239438bdb8b1a92b80e37821641a76ffa
mr-phazer <christiandyrnesli@gmail.com>
2024-11-28 13:06:03 +0100

    Used new error window instead of MessageBox, for SceneSaver.Save()'s try/catch

--- Editors/ImportExportEditor/Editors.ImportExport/Exporting/Exporters/RmvToGltf/Helpers/GltfSceneSaver.cs
+++ Editors/ImportExportEditor/Editors.ImportExport/Exporting/Exporters/RmvToGltf/Helpers/GltfSceneSaver.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Shared.Core.ErrorHandling.Exceptions;
 using SharpGLTF.Schema2;
 
 namespace Editors.ImportExport.Exporting.Exporters.RmvToGltf.Helpers
@@ -11,15 +12,22 @@ namespace Editors.ImportExport.Exporting.Exporters.RmvToGltf.Helpers
 
     public class GltfSceneSaver : IGltfSceneSaver
     {
-        public void Save(ModelRoot modelRoot, string fullSystemPath)
+        private readonly IExceptionService _exceptionService;
+
+        public GltfSceneSaver(IExceptionService exceptionService)
         {
+            _exceptionService = exceptionService;
+        }
+
+        public void Save(ModelRoot modelRoot, string fullSystemPath)
+        {        
             try
             {
                 modelRoot.SaveGLTF(fullSystemPath);
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                MessageBox.Show($"Error saving GLTF file. SharpGLTF Error:\n{e.Message}");
+                _exceptionService.CreateDialog(ex);
             }
         }
     }
