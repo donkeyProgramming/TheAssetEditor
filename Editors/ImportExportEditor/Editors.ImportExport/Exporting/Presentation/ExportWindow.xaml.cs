c5ccae1d3ef02eed503076e22448b884a0068868
mr-phazer <christiandyrnesli@gmail.com>
2024-11-28 01:21:00 +0100

    Add: Advanved Export Dialog Checks for Invalid path

--- Editors/ImportExportEditor/Editors.ImportExport/Exporting/Presentation/ExportWindow.xaml.cs
+++ Editors/ImportExportEditor/Editors.ImportExport/Exporting/Presentation/ExportWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Xna.Framework.Graphics;
 using Shared.Core.PackFiles.Models;
 
 namespace Editors.ImportExport.Exporting.Presentation
@@ -24,8 +26,64 @@ namespace Editors.ImportExport.Exporting.Presentation
 
         private void ExportButton_Click(object sender, RoutedEventArgs e)
         {
+            if (!PathValidator.IsValid(_viewModel.SystemPath))
+                return;
+
             _viewModel.Export();
             Close();
+        }
+    }
+
+    // TODO: should be moved to a shared library
+    public static class PathValidator
+    {
+        public static bool IsValid(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                return false;
+            }
+
+            if (!IsValidFileName(fileName))
+            {
+                return false;
+            }
+
+            if (!IsValidFolder(fileName))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsValidFileName(string fileName)
+        {
+            char[] invalidChars = Path.GetInvalidFileNameChars();
+            var fileNamOnly = Path.GetFileName(fileName);
+            foreach (char c in fileNamOnly)
+            {
+                if (Array.Exists(invalidChars, invalidChar => invalidChar == c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsValidFolder(string fileName)
+        {
+            char[] invalidChars = Path.GetInvalidPathChars();
+            var folder = Path.GetDirectoryName(fileName);
+            foreach (char c in folder)
+            {
+                if (Array.Exists(invalidChars, invalidChar => invalidChar == c))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
