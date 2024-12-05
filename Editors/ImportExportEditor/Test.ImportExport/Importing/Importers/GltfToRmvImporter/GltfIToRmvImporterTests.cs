2019282219e55a1c1f96bf0f7134582700de8c0a
mr-phazer <christiandyrnesli@gmail.com>
2024-12-05 20:09:07 +0100

    Add: Gltf Import Unit Testing

---
+++ Editors/ImportExportEditor/Test.ImportExport/Importing/Importers/GltfToRmvImporter/GltfIToRmvImporterTests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Editors.ImportExport.Importing.Importers.GltfToRmv;
+using Editors.ImportExport.Importing.Importers.GltfToRmv.Helper;
+using Moq;
+using Shared.Core.Events;
+using Shared.Core.PackFiles;
+using Shared.Core.PackFiles.Models;
+using Shared.Core.Services;
+using Shared.GameFormats.RigidModel;
+using Shared.TestUtility;
+using Shared.Ui.BaseDialogs.StandardDialog;
+using SharpGLTF.Schema2;
+using Test.ImportExport.Exporting.Exporters.RmvToGlft;
+
+namespace Test.ImportExport.Importing.Importers.GltfImporterTest
+{
+    class TestData
+    {
+        public static readonly string InputGtlfFile = PathHelper.FileFromDataFolder(@"Karl_Gltf_Resaved_From_Blender\karl_franz_imported_and_saved_to_blendfile_and_exported_to_gltf_from_blender.gltf");
+        public static readonly string InputPack = PathHelper.FileFromDataFolder("Humanoid01Skeleton_Pack");
+
+        public static class GltfExpected
+        {
+            public const int logicalMaterials = 4;
+            public const int nodes = 78;
+            public const int skinCount = 1;
+            public const int textureCount = 12;
+            public const int logicalMeshes = 4;
+            public const int primtivesMesh0 = 1;
+            public const int primtivesMesh0IndexCount = 25539;
+        }
+
+        public static class Rmv2Expected
+        {
+            public const string skeletonName = "humanoid01";
+            public const int lodCount = 1;
+            public const int lod0MeshCount = 4;
+            public const int Lod0Mesh0IndexCount = 25539;
+            public const int Lod0Mesh0VertexCount = 6397;
+        }
+    }
+
+    // Tests Full Import (Importer.Import())
+    class GltfToRmv2ImporterTest
+    {     
+        [Test]
+        public void Test()
+        {
+            // Arrange 
+            var pfs = PackFileSerivceTestHelper.Create(TestData.InputPack);
+            var standardDialog = new Mock<IStandardDialogs>();
+            var sceneLoader = new GltfSceneLoader(standardDialog.Object);            
+            var importer = new GltfImporter(pfs, standardDialog.Object);
+
+            var packFileContainer = new PackFileContainer("new");
+            var settings = new GltfImporterSettings(TestData.InputGtlfFile, "skeletons", packFileContainer, true, true, true);
+
+            // Act
+            // Do full import
+            importer.Import(settings);
+            var rmv2FileName = @$"skeletons\{Path.GetFileNameWithoutExtension(TestData.InputGtlfFile)}.rigid_model_v2".ToLower();
+            var isPackFileAdded = packFileContainer.FileList.ContainsKey(rmv2FileName);
+
+            // Assert
+            // Last step of Importer, is adding the file to PackFileContainer, so we check wether this has happened
+            Assert.That(isPackFileAdded, Is.EqualTo(true));
+        }
+    }
+
+    // Tests some Components of Importer pipeline
+    class GltfToRmv2ImporterComponentTest
+    {       
+
+        [Test]
+        public void Test()
+        {
+            // Arrange 
+            var pfs = PackFileSerivceTestHelper.Create(TestData.InputPack);
+            var standardDialog = new Mock<IStandardDialogs>();
+            var sceneLoader = new GltfSceneLoader(standardDialog.Object);            
+            var importer = new GltfImporter(pfs, standardDialog.Object);
+
+            var packFileContainer = new PackFileContainer("new");
+            var settings = new GltfImporterSettings(TestData.InputGtlfFile, "skeletons", packFileContainer, true, true, true);
+
+            // Act....          
+            var modelRoot = sceneLoader.Load(settings);
+
+            // Assert            
+            // Assert API correct load
+            Assert.That(modelRoot, Is.Not.Null);
+            Assert.That(modelRoot.LogicalMaterials!.Count(), Is.EqualTo(TestData.GltfExpected.logicalMaterials));
+            Assert.That(modelRoot.LogicalNodes!.Count(), Is.EqualTo(TestData.GltfExpected.nodes));
+            Assert.That(modelRoot.LogicalSkins!.Count(), Is.EqualTo(TestData.GltfExpected.skinCount));
+            Assert.That(modelRoot.LogicalTextures!.Count(), Is.EqualTo(TestData.GltfExpected.textureCount));
+            Assert.That(modelRoot.LogicalMeshes!.Count(), Is.EqualTo(TestData.GltfExpected.logicalMeshes));
+            Assert.That(modelRoot.LogicalMeshes[0].Primitives!.Count(), Is.EqualTo(TestData.GltfExpected.primtivesMesh0));
+            Assert.That(modelRoot.LogicalMeshes[0].Primitives[0].GetIndices().Count(), Is.EqualTo(TestData.GltfExpected.primtivesMesh0IndexCount));
+
+            // Assert
+            // Test Importer code, check if Rmv2 file is created correctly
+            var rmv2Mesh = RmvMeshBuilder.Build(settings, modelRoot);
+            Assert.That(rmv2Mesh, Is.Not.Null);
+            Assert.That(rmv2Mesh.Header.SkeletonName, Is.EqualTo(TestData.Rmv2Expected.skeletonName));
+            Assert.That(rmv2Mesh.ModelList, Is.Not.Null);
+            Assert.That(rmv2Mesh.ModelList.Length, Is.EqualTo(TestData.Rmv2Expected.lodCount));
+            Assert.That(rmv2Mesh.ModelList[0].Length, Is.EqualTo(TestData.Rmv2Expected.lod0MeshCount));
+            Assert.That(rmv2Mesh.ModelList[0][0].Mesh, Is.Not.Null);
+            Assert.That(rmv2Mesh.ModelList[0][0].Material, Is.Not.Null);
+            Assert.That(rmv2Mesh.ModelList[0][0].Material.MaterialId, Is.EqualTo(ModelMaterialEnum.weighted));
+            Assert.That(rmv2Mesh.ModelList[0][0].Material.BinaryVertexFormat, Is.EqualTo(VertexFormat.Cinematic));
+            Assert.That(rmv2Mesh.ModelList[0][0].Mesh.IndexList, Is.Not.Null);
+            Assert.That(rmv2Mesh.ModelList[0][0].Mesh.IndexList.Length, Is.EqualTo(TestData.Rmv2Expected.Lod0Mesh0IndexCount));
+            Assert.That(rmv2Mesh.ModelList[0][0].Mesh.VertexList.Length, Is.EqualTo(TestData.Rmv2Expected.Lod0Mesh0VertexCount));
+        }
+    }
+}
--- Testing/Shared/TestUtility/PathHelper.cs
+++ Testing/Shared/TestUtility/PathHelper.cs
@@ -1,9 +1,40 @@
-﻿using System.Text;
+﻿using System.ComponentModel.Design.Serialization;
+using System.Text;
 
 namespace Shared.TestUtility
 {
     public static class PathHelper
     {
+        /// <summary>
+        /// Find the "AssetEditor" folder from the test directory and return the path to the file
+        /// Probably superior to the hardcoded path in the original code
+        /// </summary>        
+        public static string FileFromDataFolder(string fileName, string rootDir = "TheAssetEditor")
+        {
+            var currentDirectory = TestContext.CurrentContext.TestDirectory;
+            if (string.IsNullOrEmpty(currentDirectory))
+                return "";
+
+            while (true)
+            {
+                fileName = Path.GetFileName(currentDirectory); // get last foldername
+                if (string.IsNullOrEmpty(fileName))
+                    return "";
+
+                if (fileName.ToLower() == rootDir.ToLower())
+                    break;
+
+                currentDirectory = Path.GetDirectoryName(currentDirectory); // go one folder UP
+                if (string.IsNullOrEmpty(currentDirectory))  // reached root, nothing foun              
+                    return "";
+            }
+
+            var fullPath = currentDirectory += @"\data\" + fileName;
+
+            return fullPath;
+        }
+
+
         public static string File(string fileName)
         {
             var fullPath = Path.GetFullPath(@"..\..\..\..\..\Data\" + fileName);
