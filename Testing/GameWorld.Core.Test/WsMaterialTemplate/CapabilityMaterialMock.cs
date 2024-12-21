c00d5f0e755d611c5239a0d1d30775e838a03975
Josh Ewart <134755835+HorseF3thers@users.noreply.github.com>
2024-12-21 11:24:48 -0600

    Fixed pharaoh wsmodel generation as well as some material issues, added some unit tests
    Added 6 unit tests to make sure the pharaoh static, weight and cinematic were being named properly. I also added three for all of the other games as their names are all the same.

---
+++ Testing/GameWorld.Core.Test/WsMaterialTemplate/CapabilityMaterialMock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameWorld.Core.Rendering.Materials.Shaders;
+using GameWorld.Core.Services;
+
+namespace Test.GameWorld.Core.WsMaterialTemplate
+{
+    public class CapabilityMaterialMock : CapabilityMaterial
+    {
+        public CapabilityMaterialMock(CapabilityMaterialsEnum materialType) : base(materialType, ShaderTypes.Pbr_SpecGloss, null)
+        {
+
+        }
+        protected override CapabilityMaterial CreateCloneInstance()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Testing/GameWorld.Core.Test/WsMaterialTemplate/WsMaterialTemplateTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using GameWorld.Core.Rendering.Materials.Serialization;
+using GameWorld.Core.Rendering.Materials.Capabilities;
+using GameWorld.Core.Rendering.Materials.Shaders;
+using Shared.Core.Settings;
+using Shared.GameFormats.RigidModel;
+using System.Diagnostics.Contracts;
+
+namespace Test.GameWorld.Core.WsMaterialTemplate
+{
+    public class WsMaterialTemplateTests
+    {
+        [Test] //Pharaoh weighted
+        public void AddTemplateHeader_ReturnCorrectFileNameWeighted()
+        {
+            var material = new CapabilityMaterialMock(CapabilityMaterialsEnum.SpecGlossPbr_Default);
+            var editor = new WsMaterialTemplateEditor(material, GameTypeEnum.Pharaoh);
+            var meshName = "testMesh";
+            var vertexFormat = UiVertexFormat.Weighted;
+
+            var result = editor.AddTemplateHeader(meshName, vertexFormat, material);
+            Assert.That(result, Is.EqualTo("testMesh_weighted_standard_2.xml.material"));
+        }
+        [Test] //Pharaoh static
+        public void AddTemplateHeader_ReturnCorrectFileNameStatic()
+        {
+            var material = new CapabilityMaterialMock(CapabilityMaterialsEnum.SpecGlossPbr_Default);
+            var editor = new WsMaterialTemplateEditor(material, GameTypeEnum.Pharaoh);
+            var meshName = "testMesh";
+            var vertexFormat = UiVertexFormat.Static;
+            var result = editor.AddTemplateHeader(meshName, vertexFormat, material);
+            Assert.That(result, Is.EqualTo("testMesh_rigid.xml.material"));
+        }
+        [Test] //Pharaoh cinematic
+        public void AddTemplateHeader_ReturnCorrectFileNameCinematic()
+        {
+            var material = new CapabilityMaterialMock(CapabilityMaterialsEnum.SpecGlossPbr_Default);
+            var editor = new WsMaterialTemplateEditor(material, GameTypeEnum.Pharaoh);
+            var meshName = "testMesh";
+            var vertexFormat = UiVertexFormat.Cinematic;
+            var result = editor.AddTemplateHeader(meshName, vertexFormat, material);
+            Assert.That(result, Is.EqualTo("testMesh_weighted_standard_4.xml.material"));
+        }
+        [Test] //All other games static no alpha
+        public void AddTemplateHeader_ReturnCorrectFileNameStaticAllOtherGames()
+        {
+            var material = new CapabilityMaterialMock(CapabilityMaterialsEnum.SpecGlossPbr_Default);
+            var editor = new WsMaterialTemplateEditor(material, GameTypeEnum.Warhammer2);
+            var meshName = "testMesh";
+            var vertexFormat = UiVertexFormat.Static;
+            var result = editor.AddTemplateHeader(meshName, vertexFormat, material);
+            Assert.That(result, Is.EqualTo("testMesh_rigid_alpha_off.xml.material"));
+        }
+        [Test] //All other games weighted no alpha
+        public void AddTemplateHeader_ReturnCorrectFileNameWeightedAllOtherGames()
+        {
+            var material = new CapabilityMaterialMock(CapabilityMaterialsEnum.SpecGlossPbr_Default);
+            var editor = new WsMaterialTemplateEditor(material, GameTypeEnum.Warhammer2);
+            var meshName = "testMesh";
+            var vertexFormat = UiVertexFormat.Weighted;
+            var result = editor.AddTemplateHeader(meshName, vertexFormat, material);
+            Assert.That(result, Is.EqualTo("testMesh_weighted2_alpha_off.xml.material"));
+        }
+        [Test] //All other games cinematic no alpha
+        public void AddTemplateHeader_ReturnCorrectFileNameCinematicAllOtherGames()
+        {
+            var material = new CapabilityMaterialMock(CapabilityMaterialsEnum.SpecGlossPbr_Default);
+            var editor = new WsMaterialTemplateEditor(material, GameTypeEnum.Warhammer2);
+            var meshName = "testMesh";
+            var vertexFormat = UiVertexFormat.Cinematic;
+            var result = editor.AddTemplateHeader(meshName, vertexFormat, material);
+            Assert.That(result, Is.EqualTo("testMesh_weighted4_alpha_off.xml.material"));
+        }
+
+    }
+}
