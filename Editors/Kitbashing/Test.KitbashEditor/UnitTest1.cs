64ad98aa04eda83a4010acb47f9563ab3128729d
ole kristian homelien <ole_kristian92@hotmail.com>
2024-12-25 21:16:28 +0100

    Moved kitbash editor to own folder and created test project

---
+++ Editors/Kitbashing/Test.KitbashEditor/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace Test.KitbashEditor
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
