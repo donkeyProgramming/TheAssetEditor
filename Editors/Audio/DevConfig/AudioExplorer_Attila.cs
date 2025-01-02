5616e3e435f954d68be4f5531c5e573ba5b1cf56
Pear <61670316+Pear-231@users.noreply.github.com>
2025-01-02 22:19:35 +0000

    Change Notes: - Added Atilla Audio Explorer dev stuff.

--- Editors/Audio/DevConfig/AudioExplorer_Attila.cs
+++ Editors/Audio/DevConfig/AudioExplorer_Attila.cs
@@ -18,7 +18,8 @@ namespace Editors.Audio.DevConfig
         public void OverrideSettings(ApplicationSettings currentSettings)
         {
             currentSettings.CurrentGame = GameTypeEnum.Attila;
-            currentSettings.ShowCAWemFiles = false;
+            currentSettings.LoadCaPacksByDefault = true;
+            currentSettings.ShowCAWemFiles = true;
         }
 
         public void OpenFileOnLoad()
