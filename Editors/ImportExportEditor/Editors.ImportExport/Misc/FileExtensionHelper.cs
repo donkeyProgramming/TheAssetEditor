83b2544cc655d87acfcdedfcdd64f9da2f6fe86d
mr-phazer <christiandyrnesli@gmail.com>
2025-02-03 22:21:50 +0100

    Change/Correction: FileExtensioin helper uses string.EndsWith(), instead of "Contains"

--- Editors/ImportExportEditor/Editors.ImportExport/Misc/FileExtensionHelper.cs
+++ Editors/ImportExportEditor/Editors.ImportExport/Misc/FileExtensionHelper.cs
@@ -10,26 +10,26 @@ namespace Editors.ImportExport.Misc
 
         public static bool IsDdsFile(string fileName)
         {
-            var isDdsFile = fileName.Contains(".dds", StringComparison.InvariantCultureIgnoreCase);
+            var isDdsFile = fileName.EndsWith(".dds", StringComparison.InvariantCultureIgnoreCase);
             return isDdsFile;
         }
 
         public static bool IsDdsMaterialFile(string fileName)
         {
             var isDdsFile = IsDdsFile(fileName);
-            var isMaterialFile = fileName.Contains("material", StringComparison.InvariantCultureIgnoreCase);
+            var isMaterialFile = fileName.EndsWith("material", StringComparison.InvariantCultureIgnoreCase);
             return isDdsFile && isMaterialFile;
         }
 
         public static bool IsRmvFile(string fileName)
         {
-            var isRmv = fileName.Contains(".rigid_model_v2", StringComparison.InvariantCultureIgnoreCase);
+            var isRmv = fileName.EndsWith(".rigid_model_v2", StringComparison.InvariantCultureIgnoreCase);
             return isRmv;
         }
 
         public static bool IsWsModelFile(string fileName)
         {
-            var isRmv = fileName.Contains(".wsmodel", StringComparison.InvariantCultureIgnoreCase);
+            var isRmv = fileName.EndsWith(".wsmodel", StringComparison.InvariantCultureIgnoreCase);
             return isRmv;
         }
     }
