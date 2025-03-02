bfcb40c74fcdcff4981d50c7c3e5e18a4794b6aa
Pear <61670316+Pear-231@users.noreply.github.com>
2025-03-02 12:26:01 +0000

    Change Notes: - Removed old audio compiler.

--- Editors/Audio/DependencyInjectionContainer.cs
+++ Editors/Audio/DependencyInjectionContainer.cs
@@ -1,14 +1,9 @@
 ﻿using Audio.AudioExplorer;
-using Audio.Compiler;
 using Editors.Audio.AudioEditor;
 using Editors.Audio.AudioEditor.AudioProjectCompiler;
 using Editors.Audio.AudioEditor.AudioProjectData.AudioProjectService;
 using Editors.Audio.AudioEditor.NewAudioProject;
 using Editors.Audio.AudioExplorer;
-using Editors.Audio.BnkCompiler;
-using Editors.Audio.BnkCompiler.ObjectGeneration;
-using Editors.Audio.BnkCompiler.ObjectGeneration.Warhammer3;
-using Editors.Audio.Compiler;
 using Editors.Audio.Storage;
 using Editors.Audio.Utility;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,23 +21,16 @@ namespace Editors.Audio
             serviceCollection.AddScoped<AudioExplorerView>();
             serviceCollection.AddScoped<AudioExplorerViewModel>();
 
-            serviceCollection.AddScoped<CompilerView>();
-            serviceCollection.AddScoped<CompilerViewModel>();
-
-            serviceCollection.AddTransient<NewAudioProjectWindow>();
-            serviceCollection.AddScoped<NewAudioProjectViewModel>();
-
             serviceCollection.AddScoped<AudioEditorViewModel>();
             serviceCollection.AddScoped<AudioEditorView>();
+            serviceCollection.AddScoped<NewAudioProjectViewModel>();
+            serviceCollection.AddTransient<NewAudioProjectWindow>();
+            serviceCollection.AddTransient<AudioProjectCompiler>();
 
             serviceCollection.AddScoped<IAudioProjectService, AudioProjectService>();
 
             serviceCollection.AddScoped<RepositoryProvider, CreateRepositoryFromAllPackFiles>();
             serviceCollection.AddScoped<IAudioRepository, AudioRepository>();
-
-
-
-            serviceCollection.AddTransient<AudioProjectCompiler>();
 
             serviceCollection.AddTransient<VgStreamWrapper>();
             serviceCollection.AddTransient<WemGenerator>();
@@ -50,21 +38,6 @@ namespace Editors.Audio
             serviceCollection.AddTransient<DatLoader>();
             serviceCollection.AddTransient<BnkParser>();
             serviceCollection.AddTransient<SoundPlayer>();
-
-            serviceCollection.AddScoped<IWwiseHircGenerator, ActionGenerator>();
-            serviceCollection.AddScoped<IWwiseHircGenerator, EventGenerator>();
-            serviceCollection.AddScoped<IWwiseHircGenerator, SoundGenerator>();
-            serviceCollection.AddScoped<IWwiseHircGenerator, ActorMixerGenerator>();
-            serviceCollection.AddScoped<IWwiseHircGenerator, RandomContainerGenerator>();
-            serviceCollection.AddScoped<IWwiseHircGenerator, DialogueEventGenerator>();
-
-            serviceCollection.AddScoped<HircBuilder>();
-            serviceCollection.AddScoped<BnkHeaderBuilder>();
-            serviceCollection.AddScoped<CompilerService>();
-            serviceCollection.AddScoped<ProjectLoader>();
-            serviceCollection.AddScoped<AudioFileImporter>();
-            serviceCollection.AddScoped<BnkCompiler.Compiler>();
-            serviceCollection.AddScoped<ResultHandler>();
 
             RegisterAllAsInterface<IDeveloperConfiguration>(serviceCollection, ServiceLifetime.Transient);
         }
@@ -74,11 +47,6 @@ namespace Editors.Audio
             EditorInfoBuilder
                 .Create<AudioExplorerViewModel, AudioExplorerView>(EditorEnums.AudioExplorer_Editor)
                 .AddToToolbar("Audio Explorer")
-                .Build(factory);
-
-            EditorInfoBuilder
-                .Create<CompilerViewModel, CompilerView>(EditorEnums.AudioCompiler_Editor)
-                .AddToToolbar("Audio Compiler")
                 .Build(factory);
 
             EditorInfoBuilder
