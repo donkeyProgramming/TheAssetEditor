fe49b5774249e6d0cbe17e5d116652f0f3911838
热心市民石先生 <1249467256@qq.com>
2026-04-13 18:18:48 +0800

    Make directional light follow camera rotation
    Adds camera Yaw/Pitch to the directional light rotation angles,
    so the light source moves with the camera viewport. This improves
    model visibility when viewing from different angles, as the lit
    side of the model remains visible regardless of camera position.

--- GameWorld/View3D/Components/Rendering/CommonShaderParameterBuilder.cs
+++ GameWorld/View3D/Components/Rendering/CommonShaderParameterBuilder.cs
@@ -7,15 +7,20 @@ namespace GameWorld.Core.Components.Rendering
     {
         public static CommonShaderParameters Build(ArcBallCamera camera, SceneRenderParametersStore sceneLightParameters)
         {
+            // Light follows camera rotation for better model visibility
+            float dirLightRotX = MathHelper.ToRadians(sceneLightParameters.DirLightRotationDegrees_X) + camera.Pitch;
+            float dirLightRotY = MathHelper.ToRadians(sceneLightParameters.DirLightRotationDegrees_Y) + camera.Yaw;
+            float envLightRotY = MathHelper.ToRadians(sceneLightParameters.EnvLightRotationDegrees_Y) + camera.Yaw;
+
             var commonShaderParameters = new CommonShaderParameters(
                 camera.ViewMatrix,
                 camera.ProjectionMatrix,
                 camera.Position,
                 camera.LookAt,
 
-                MathHelper.ToRadians(sceneLightParameters.EnvLightRotationDegrees_Y),
-                MathHelper.ToRadians(sceneLightParameters.DirLightRotationDegrees_X),
-                MathHelper.ToRadians(sceneLightParameters.DirLightRotationDegrees_Y),
+                envLightRotY,
+                dirLightRotX,
+                dirLightRotY,
                 sceneLightParameters.LightIntensityMult,
 
                 [sceneLightParameters.FactionColour0, sceneLightParameters.FactionColour1, sceneLightParameters.FactionColour2],
