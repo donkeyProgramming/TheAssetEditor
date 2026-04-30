9e8bb5ae7dbc17d0fc36bc5ea9ed33a5e323521f
ole kristian homelien <ole_kristian92@hotmail.com>
2026-04-30 22:03:48 +0200

    tests

--- Shared/SharedCore/Shared.CoreTest/PackFiles/Serialization/PackFileContainerCacheHelperTests.cs
+++ Shared/SharedCore/Shared.CoreTest/PackFiles/Serialization/PackFileContainerCacheHelperTests.cs
@@ -236,11 +236,206 @@ namespace Shared.CoreTest.PackFiles.Serialization
         }
 
         [Test]
-        public void LoadCache_ReturnsNullForCorruptDb()
+        public void TryLoadFromCache_ReturnsContainerWhenValid()
         {
-            File.WriteAllBytes(_dbFilePath, [0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00]);
-            var result = PackFileContainerCacheHelper.LoadContainerFromCache(_dbFilePath, "fp");
+            var container = new PackFileContainer("TryLoad Test")
+            {
+                SystemFilePath = @"c:\game\data"
+            };
+
+            var parent = new PackedFileSourceParent { FilePath = @"c:\game\data\pack.pack" };
+            container.AddOrUpdateFile("test\\file.txt", new PackFile("file.txt",
+                new PackedFileSource(parent, 0, 100, false, false, CompressionFormat.None, 0)));
+
+            var dbOptions = CreateTestDbOptions();
+            PackFileContainerCacheHelper.SaveCache("fp_try", container, dbOptions);
+
+            var result = PackFileContainerCacheHelper.TryLoadFromCache(_dbFilePath, "fp_try");
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Name, Is.EqualTo("TryLoad Test"));
+        }
+
+        [Test]
+        public void TryLoadFromCache_ReturnsNullForMissingFile()
+        {
+            var result = PackFileContainerCacheHelper.TryLoadFromCache(
+                Path.Combine(_tempDir, "does_not_exist.db"), "fp");
             Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void TryLoadFromCache_ReturnsNullForCorruptFile()
+        {
+            File.WriteAllBytes(_dbFilePath, [0xFF, 0xFE, 0x00, 0x01]);
+            var result = PackFileContainerCacheHelper.TryLoadFromCache(_dbFilePath, "fp");
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void SaveCache_PreservesEncryptedFlag()
+        {
+            var container = new PackFileContainer("Encrypted Test")
+            {
+                SystemFilePath = @"c:\game"
+            };
+
+            var parent = new PackedFileSourceParent { FilePath = @"c:\game\encrypted.pack" };
+            container.AddOrUpdateFile("secret\\data.bin", new PackFile("data.bin",
+                new PackedFileSource(parent, 0, 500, isEncrypted: true, isCompressed: false, CompressionFormat.None, 0)));
+
+            var dbOptions = CreateTestDbOptions();
+            PackFileContainerCacheHelper.SaveCache("fp", container, dbOptions);
+            var loaded = PackFileContainerCacheHelper.LoadContainerFromCache(dbOptions, "fp");
+
+            var source = (PackedFileSource)loaded!.FindFile("secret\\data.bin")!.DataSource;
+            Assert.That(source.IsEncrypted, Is.True);
+        }
+
+        [Test]
+        public void SaveCache_EmptyContainer_RoundTrips()
+        {
+            var container = new PackFileContainer("Empty Pack")
+            {
+                SystemFilePath = @"c:\game\empty"
+            };
+
+            var dbOptions = CreateTestDbOptions();
+            PackFileContainerCacheHelper.SaveCache("fp_empty", container, dbOptions);
+            var loaded = PackFileContainerCacheHelper.LoadContainerFromCache(dbOptions, "fp_empty");
+
+            Assert.That(loaded, Is.Not.Null);
+            Assert.That(loaded.Name, Is.EqualTo("Empty Pack"));
+            Assert.That(loaded.GetFileCount(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SaveCache_OverwritesExistingCache()
+        {
+            var parent = new PackedFileSourceParent { FilePath = @"c:\game\pack.pack" };
+            var dbOptions = CreateTestDbOptions();
+
+            // Save first version
+            var container1 = new PackFileContainer("Version1") { SystemFilePath = @"c:\game" };
+            container1.AddOrUpdateFile("old.txt", new PackFile("old.txt",
+                new PackedFileSource(parent, 0, 10, false, false, CompressionFormat.None, 0)));
+            PackFileContainerCacheHelper.SaveCache("fp1", container1, dbOptions);
+
+            // Save second version (same db path)
+            var container2 = new PackFileContainer("Version2") { SystemFilePath = @"c:\game" };
+            container2.AddOrUpdateFile("new.txt", new PackFile("new.txt",
+                new PackedFileSource(parent, 0, 20, false, false, CompressionFormat.None, 0)));
+            PackFileContainerCacheHelper.SaveCache("fp2", container2, dbOptions);
+
+            // Old fingerprint should fail
+            var oldResult = PackFileContainerCacheHelper.LoadContainerFromCache(dbOptions, "fp1");
+            Assert.That(oldResult, Is.Null);
+
+            // New fingerprint should work
+            var newResult = PackFileContainerCacheHelper.LoadContainerFromCache(dbOptions, "fp2");
+            Assert.That(newResult, Is.Not.Null);
+            Assert.That(newResult.Name, Is.EqualTo("Version2"));
+            Assert.That(newResult.GetFileCount(), Is.EqualTo(1));
+            Assert.That(newResult.FindFile("new.txt"), Is.Not.Null);
+        }
+
+        [Test]
+        public void SaveCache_StoresFolderPathCorrectly()
+        {
+            var container = new PackFileContainer("FolderPath Test")
+            {
+                SystemFilePath = @"c:\game"
+            };
+
+            var parent = new PackedFileSourceParent { FilePath = @"c:\game\pack.pack" };
+            container.AddOrUpdateFile("a\\b\\c\\file.txt", new PackFile("file.txt",
+                new PackedFileSource(parent, 0, 10, false, false, CompressionFormat.None, 0)));
+            container.AddOrUpdateFile("root_file.txt", new PackFile("root_file.txt",
+                new PackedFileSource(parent, 10, 20, false, false, CompressionFormat.None, 0)));
+
+            var dbOptions = CreateTestDbOptions();
+            PackFileContainerCacheHelper.SaveCache("fp", container, dbOptions);
+
+            // Verify via the CachedPackFileContainer's GetDirectoryContent
+            var loaded = PackFileContainerCacheHelper.LoadContainerFromCache(dbOptions, "fp");
+            Assert.That(loaded, Is.Not.Null);
+
+            var rootContent = loaded.GetDirectoryContent("");
+            Assert.That(rootContent.Files.Any(f => f.FileName == "root_file.txt"), Is.True);
+            Assert.That(rootContent.SubFolders, Does.Contain("a"));
+
+            var deepContent = loaded.GetDirectoryContent("a\\b\\c");
+            Assert.That(deepContent.Files.Any(f => f.FileName == "file.txt"), Is.True);
+        }
+
+        [Test]
+        public void SaveCache_SkipsNonPackedFileSources()
+        {
+            var container = new PackFileContainer("Mixed Sources")
+            {
+                SystemFilePath = @"c:\game"
+            };
+
+            var parent = new PackedFileSourceParent { FilePath = @"c:\game\pack.pack" };
+            container.AddOrUpdateFile("packed.txt", new PackFile("packed.txt",
+                new PackedFileSource(parent, 0, 10, false, false, CompressionFormat.None, 0)));
+            container.AddOrUpdateFile("memory.txt", new PackFile("memory.txt",
+                new MemorySource([1, 2, 3])));
+
+            var dbOptions = CreateTestDbOptions();
+            PackFileContainerCacheHelper.SaveCache("fp", container, dbOptions);
+            var loaded = PackFileContainerCacheHelper.LoadContainerFromCache(dbOptions, "fp");
+
+            Assert.That(loaded, Is.Not.Null);
+            Assert.That(loaded.GetFileCount(), Is.EqualTo(1));
+            Assert.That(loaded.FindFile("packed.txt"), Is.Not.Null);
+            Assert.That(loaded.FindFile("memory.txt"), Is.Null);
+        }
+
+        [Test]
+        public void ComputeFingerprint_IgnoresMissingPackFiles()
+        {
+            var packDir = Path.Combine(_tempDir, "partial");
+            Directory.CreateDirectory(packDir);
+            File.WriteAllText(Path.Combine(packDir, "exists.pack"), "data");
+
+            var packFiles = new List<string> { "exists.pack", "missing.pack" };
+            var fp = PackFileContainerCacheHelper.ComputeFingerprint(packDir, packFiles);
+
+            Assert.That(fp, Is.Not.Null.And.Not.Empty);
+
+            // Same result regardless of missing file in list
+            var fp2 = PackFileContainerCacheHelper.ComputeFingerprint(packDir, new List<string> { "exists.pack", "missing.pack" });
+            Assert.That(fp, Is.EqualTo(fp2));
+        }
+
+        [Test]
+        public void ComputeFingerprint_OrderIndependent()
+        {
+            var packDir = Path.Combine(_tempDir, "order_test");
+            Directory.CreateDirectory(packDir);
+            File.WriteAllText(Path.Combine(packDir, "alpha.pack"), "aaa");
+            File.WriteAllText(Path.Combine(packDir, "beta.pack"), "bbb");
+            File.WriteAllText(Path.Combine(packDir, "gamma.pack"), "ccc");
+
+            var fp1 = PackFileContainerCacheHelper.ComputeFingerprint(packDir,
+                new List<string> { "gamma.pack", "alpha.pack", "beta.pack" });
+            var fp2 = PackFileContainerCacheHelper.ComputeFingerprint(packDir,
+                new List<string> { "alpha.pack", "beta.pack", "gamma.pack" });
+            var fp3 = PackFileContainerCacheHelper.ComputeFingerprint(packDir,
+                new List<string> { "beta.pack", "gamma.pack", "alpha.pack" });
+
+            Assert.That(fp1, Is.EqualTo(fp2));
+            Assert.That(fp2, Is.EqualTo(fp3));
+        }
+
+        [Test]
+        public void GetCacheFilePath_SanitizesInvalidChars()
+        {
+            var path = PackFileContainerCacheHelper.GetCacheFilePath(@"c:\game", "Game:Name/With<Bad>Chars", "abc123");
+            var fileName = Path.GetFileName(path);
+
+            Assert.That(fileName.IndexOfAny(Path.GetInvalidFileNameChars()), Is.EqualTo(-1));
+            Assert.That(path.EndsWith(".db"), Is.True);
         }
     }
 }
