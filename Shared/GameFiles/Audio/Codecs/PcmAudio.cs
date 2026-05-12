07e328b10bb5170d90fe4f0afc798a171cac202f
Pear-231 <61670316+Pear-231@users.noreply.github.com>
2026-05-12 21:31:23 +0100

    Added native WAV

---
+++ Shared/GameFiles/Audio/Codecs/PcmAudio.cs
@@ -0,0 +1,14 @@
+﻿using Shared.ByteParsing;
+
+namespace Shared.GameFormats.Audio.Codecs
+{
+    public class PcmAudio
+    {
+        public ushort BitsPerSample { get; set; }
+        public ushort Channels { get; set; }
+        public byte[] Data { get; set; } = [];
+        public uint SampleRate { get; set; }
+
+        public int SampleCount => Data.Length / (BitsPerSample / BitHelper.BitsPerByte) / Channels;
+    }
+}
