f383dc3d7f8abeefdf80107b88419f0e066abc66
ole kristian homelien <ole_kristian92@hotmail.com>
2026-06-26 15:14:24 +0200

    Bugfix

--- AssetEditor/UiCommands/ImportReferencePackCommand.cs
+++ AssetEditor/UiCommands/ImportReferencePackCommand.cs
@@ -3,7 +3,6 @@ using Shared.Core.Events;
 using Shared.Core.PackFiles;
 using Shared.Core.PackFiles.Models;
 using Shared.Core.PackFiles.Utility;
-using Shared.Core.Services;
 
 namespace AssetEditor.UiCommands
 {
@@ -11,19 +10,12 @@ namespace AssetEditor.UiCommands
     {
         private readonly IPackFileService _packFileService;
         private readonly IPackFileContainerLoader _packFileContainerLoader;
-        private readonly ISystemFolderContainerFactory _systemFolderContainerFactory;
-        private readonly IStandardDialogs _standardDialogs;
-
         public ImportReferencePackCommand(
             IPackFileService packFileService,
-            IPackFileContainerLoader packFileContainerLoader,
-            ISystemFolderContainerFactory systemFolderContainerFactory,
-            IStandardDialogs standardDialogs)
+            IPackFileContainerLoader packFileContainerLoader)
         {
             _packFileService = packFileService;
             _packFileContainerLoader = packFileContainerLoader;
-            _systemFolderContainerFactory = systemFolderContainerFactory;
-            _standardDialogs = standardDialogs;
         }
 
         public void Execute()
@@ -37,7 +29,7 @@ namespace AssetEditor.UiCommands
                 return;
 
             var container = _packFileContainerLoader.CreateFromPackFile(PackFileContainerType.Normal, dialog.FileName, true);
-            _packFileService.AddContainer(container, true);
+            _packFileService.AddContainer(container, false);
         }
     }
 }
